c19ec702ecd81606baed2e4111eadbbde9b0cfbb
Ivaylo Grahlyov <ivaylo.grahlyov@mentormate.com>
2025-08-08 18:10:14 +0300

    fix: remove debug log statement

--- game-of-life-api/src/common/decorators/current-user.decorator.ts
+++ game-of-life-api/src/common/decorators/current-user.decorator.ts
@@ -3,7 +3,6 @@ import { Request } from 'express';
 
 export const CurrentUser = createParamDecorator((_, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest<Request>();
-  Logger.error({ user: request.user }, 'CurrentUser');
 
   return request?.user; // from passport-jwt
 });
